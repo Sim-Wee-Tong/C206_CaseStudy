69255cd9faf395688ad9400cd8d8f895ac70e985
65811 <65811@LAPTOP-QDG5FPKF>
2021-08-09 10:52:16 +0800

    Update

---
+++ C206_CaseStudy/src/CCAcategory.java
@@ -0,0 +1,19 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 65811, 9 Aug 2021 10:51:17 am
+ */
+
+public class CCAcategory {
+	private String category;
+	
+	public CCAcategory(String category) {
+		this.category = category;
+	}
+	public String getCategory() {
+		return category;
+	}
+}
