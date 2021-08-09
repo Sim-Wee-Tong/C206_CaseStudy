6c3faa4ec156e993a2ba29d7ccc42016e69ff328
SIM WEE TONG RP <19020438@myrp.edu.sg>
2021-08-09 12:01:26 +0800

    Updated main page for selection

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -9,7 +9,7 @@ public class C206_CaseStudy {
 		
 		int role = 0;
 		
-		while (role != 3) {
+		while (role != 4) {
 			C206_CaseStudy.roleMenu(); // Displays role for CCA Coordinator and Parent/Student
 			role = Helper.readInt("Enter role > ");
 			
@@ -54,78 +54,72 @@ public class C206_CaseStudy {
 			}
 			
 			else if (role == 2) {
-				// Parent-Student role
-				System.out.println("Parent/Student role");
-				C206_CaseStudy.gradeMenu(); // Displays grade for parents (<= p3) and students (>= p4)
-				int choice = 0;
+				// Parent role
+				System.out.println("Parent");
+				C206_CaseStudy.parentMenu(); // Displays menu for parents 
 				
-				while (choice != 3) {
+				int option = 0;
+				
+				while (option != 4) {
 					
-					choice = Helper.readInt("Enter choice (Parent/Student)? >");
+					option = Helper.readInt("Enter option > ");
 					
-					if (choice == 1) {
-						// Parent
-						System.out.println("Parent");
-						
-						int option = 0;
-						
-						while (option != 4) {
-							
-							if (option == 1) {
-								// View all parent
-							}
-							else if (option == 2) {
-								// Add parent
-							}
-							else if (option == 3) {
-								// Delete parent
-							}
-							else if (option == 4) {
-								// Quit
-								System.out.println("Returning to selection page...");
-							}
-							else {
-								System.out.println("Invalid Option!");
-							}
-						}
+					if (option == 1) {
+						// View all parent
+						System.out.println("View all parent");
 					}
-					else if (choice == 2) {
-						//Student
-						System.out.println("Student");
-						
-						int option = 0;
-						
-						while (option != 4) {
-							
-							if (option == 1) {
-								// View all students
-							}
-							else if (option == 2) {
-								// Add student
-							}
-							else if (option == 3) {
-								// Delete student
-							}
-							else if (option == 4) {
-								// Quit
-								System.out.println("Returning to selection page...");
-							}
-							else {
-								System.out.println("Invalid Option!");
-							}
-						}
-
+					else if (option == 2) {
+						// Add parent
+						System.out.println("Add parent");
 					}
-					else if (choice == 3) {
+					else if (option == 3) {
+						// Delete parent
+						System.out.println("Delete parent");
+					}
+					else if (option == 4) {
 						// Quit
-						System.out.println("Returning to Login page...");
+						System.out.println("Returning to selection page...");
 					}
 					else {
-						System.out.println("Invalid Choice");
+						System.out.println("Invalid Option!");
 					}
 				}
+
 			}
 			else if (role == 3) {
+				// Student role
+				System.out.println("Student");
+				C206_CaseStudy.studentMenu(); // Displays menu for students
+				
+				int option = 0;
+				
+				while (option != 4) {
+					
+					option = Helper.readInt("Enter option > ");
+					
+					if (option == 1) {
+						// View all students
+						System.out.println("View all students");
+					}
+					else if (option == 2) {
+						// Add student
+						System.out.println("Add student");
+					}
+					else if (option == 3) {
+						// Delete student
+						System.out.println("Delete student");
+					}
+					else if (option == 4) {
+						// Quit
+						System.out.println("Returning to selection page...");
+					}
+					else {
+						System.out.println("Invalid Option!");
+					}
+				}
+
+			}
+			else if (role == 4) {
 				System.out.println("Thank you for using CCA Registration System!");
 			}
 			else {
@@ -134,18 +128,47 @@ public class C206_CaseStudy {
 
 		}
 	}
+	
+	public static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
+	}
 
 	public static void roleMenu() {
 		// TODO Auto-generated method stub
-		
+		C206_CaseStudy.setHeader("ROLES");
+		System.out.println("1. CCA Coordinator");
+		System.out.println("2. Parent (If sutdent grade is in P3 and under");
+		System.out.println("3. Student");
+		System.out.println("4. Quit");
 	}
 	
 	public static void coordinatorMenu() {
-		
+		C206_CaseStudy.setHeader("CCA COORDINATOR");
+		System.out.println("1. View all CCA and Category");
+		System.out.println("2. Add CCA");
+		System.out.println("3. Delete CCA");
+		System.out.println("4. Add CCA Category");
+		System.out.println("5. Delete CCA Category");
+		System.out.println("6. Quit");
 	}
 	
-	public static void gradeMenu() {
-		
+	public static void parentMenu() {
+		C206_CaseStudy.setHeader("PARENT");
+		System.out.println("1. View all student details (For parents)");
+		System.out.println("2. Add student details");
+		System.out.println("3. Delete student details");
+		System.out.println("4. Quit");
+	}
+	
+	public static void studentMenu() {
+		C206_CaseStudy.setHeader("STUDENT");
+		System.out.println("1. View all student details");
+		System.out.println("2. Add student details");
+		System.out.println("3. Delete student details");
+		System.out.println("4. Quit");
+
 	}
 
 }
